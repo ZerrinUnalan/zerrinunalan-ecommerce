288c71d5260e65999aaee9ab2d0d94848d47d21e
Zerrin Unalan <zerunalan@gmail.com>
2024-05-24 13:33:25 +0300

    T1 tamamlandı.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // veya 'media' veya 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
