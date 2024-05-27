a271d7f214497d79dfcf32cfdfbd4191e177d8ae
Zerrin Unalan <zerunalan@gmail.com>
2024-05-27 17:19:08 +0300

    The requested folder structures have been created. (Layout Components: Header, PageContent, Footer) Tailwind has been updated.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Hero } from "../components/Hero";
+import { Header } from "../layout/Header";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+};
