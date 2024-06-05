200dd871df2538ce7d83f101801e5e79b9a2658d
Zerrin Unalan <zerunalan@gmail.com>
2024-06-06 00:10:35 +0300

    Created Clients component

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,11 +1,13 @@
 import { NavBar } from "../components/NavBar";
 import { Hero } from "../components/Hero";
+import { Clients } from "../components/Clients";
 
 export const Header = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <Clients />
     </>
   );
 };
